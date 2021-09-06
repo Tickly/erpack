29c014f98b3128e47098d3c301fa3f817d70f33b
taoguo <807081817@qq.com>
2021-09-06 11:39:09 +0800

    完善按钮功能

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,16 +1,53 @@
-import { Button } from 'ant-design-vue'
+import { Button, Popconfirm } from 'ant-design-vue'
 import './index.less'
 
 export default {
   name: 'ErpButton',
   props: {
-    type: String
+    /**
+     * 在原有的type基础上，额外扩展了几个值。
+     * success
+     * danger
+     */
+    type: String,
+    /**
+     * 在某些场景下面，点击按钮的时候，希望能够有一个二次确认的操作，所以提供了这个参数，快速设置一个提示语。
+     */
+    confirm: String,
   },
-  render(h) {
-    return h(Button, {
-      props: {
-        type: this.type
-      }
-    }, this.$slots.default)
-  }
+  methods: {
+    handleClick () {
+      this.$emit('click')
+    },
+    renderPopConfirm (h) {
+      return h(Popconfirm, {
+        props: {
+          title: this.confirm
+        },
+        on: {
+          confirm: this.handleClick
+        }
+      }, [
+        this.renderButton(h)
+      ])
+    },
+    renderButton (h) {
+      return h(Button, {
+        props: {
+          type: this.type,
+          ...this.$attrs,
+        },
+        on: {
+          click: () => {
+            if (this.confirm) return
+
+            this.handleClick()
+          }
+        }
+      }, this.$slots.default)
+    }
+  },
+  render (h) {
+    return this.confirm ? this.renderPopConfirm(h) : this.renderButton(h)
+  },
 }
