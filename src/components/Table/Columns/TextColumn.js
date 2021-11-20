bb9197ee0872d4e3447c1352517bcf893580fda7
Tickly <807081817@qq.com>
2021-11-20 19:54:29 +0800

    增加功能

--- src/components/Table/Columns/TextColumn.js
+++ src/components/Table/Columns/TextColumn.js
@@ -1,5 +1,25 @@
 import Column from './Column'
 
-export default class TextColumn extends Column{
-  
+export default class TextColumn extends Column {
+  constructor(opt) {
+    super(opt)
+
+    const { formatter } = opt
+
+    this.formatter = formatter
+  }
+
+  render(h, value) {
+    if (this.formatter) {
+      // 如果是字符串模板
+      if (typeof this.formatter === typeof '') {
+        value = value.replace('{v}', value)
+      }
+      if (this.formatter instanceof Function) {
+        value = this.formatter(value)
+      }
+    }
+
+    return value
+  }
 }
