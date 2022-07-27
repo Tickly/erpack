3013336f96bd280fb47f34f7157323a70d1da8cd
Tickly <807081817@qq.com>
2022-07-27 22:07:50 +0800

    加页面

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,44 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <div>
+  <header>123</header>
+  <aside>
+    <ul>
+      <li>button</li>
+    </ul>
+  </aside>
+  <div class="app-main">
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+
+    <HelloWorld msg="Vite + Vue" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
+<style lang="scss">
+#app {
+  // display: grid;
+  // grid-template-columns: 2;
+
+  header {
+    background-color: aquamarine;
+  }
+  aside {
+    background-color: blanchedalmond;
+  }
+  .app-main {
+    background-color: cornflowerblue;
+  }
+}
+
 .logo {
   height: 6em;
   padding: 1.5em;
